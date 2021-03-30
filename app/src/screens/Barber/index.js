28f6697146eacf65ffa0a17f85179b4fb9375e68
Jesse <lbarbosa.jessica@gmail.com>
2021-03-30 20:59:27 -0300

    barber profile part1
    criando o perfil do barbeiro parte 1.

--- app/src/screens/Barber/index.js
+++ app/src/screens/Barber/index.js
@@ -1,8 +1,36 @@
 import React, {useState, useEffect} from 'react';
 import {Text} from 'react-native';
 import {useNavigation, useRoute} from '@react-navigation/native';
+import Swiper from 'react-native-swiper';
 
-import {Container} from './styles';
+import Stars from '../../components/Stars';
+
+import FavoriteIcon from '../../assets/favorite.svg';
+import BackIcon from '../../assets/back.svg';
+
+import {
+    Container,
+    Scroller,
+
+    SwipeDot,
+    SwipeDotActive,
+    SwipeItem,
+    SwipeImage,
+    FakeSwiper,
+
+    PageBody,
+    UserInfoArea,
+    UserAvatar,
+    UserInfo,
+    UserInfoName,
+    UserFavButton,
+
+    LoadingIcon,
+    ServiceArea,
+    TestimonialArea,
+    BackButton
+
+} from './styles';
 
 import Api from '../../Api';
 
@@ -18,20 +46,83 @@ export default () => {
         stars: routes.params.stars
     });
 
+    const [loading, setLoading] = useState(false);
+
     useEffect(()=> {
         const getBarberInfo = async () => {
+            setLoading(true);
+
             let json = await Api.getBarber(userInfo.id);
             if (json.error == '') {
-                
+                setUserInfo(json.data);
             } else {
                 alert("Erro: "+ json.error);
             }
+
+            setLoading(false);
         }
     }, []);
 
+    const handleBackButton = () => {
+        navigation.goBack();
+    };
+
     return(
         <Container>
-            <Text>Barbeiro: {userInfo.name}</Text>
+            <Scroller>
+                {userInfo.photos && userInfo.photos.length > 0 ?
+                    <Swiper
+                        style={{height: 240}}
+                        dot={<SwipeDot />}
+                        activeDot={<SwipeDotActive />}
+                        paginationStyle={{top: 15, right: 15, bottom: null, left: null}}
+                        autoplay={true}
+                    >
+                        {userInfo.photos.map((item, key)=>(
+                            <SwipeItem key={key}>
+                                <SwipeImage source={{uri:item.url}} resizeMode="cover" />
+                            </SwipeItem>
+                        ))}
+                    </Swiper>
+                    :
+                    <FakeSwiper>
+
+                    </FakeSwiper>
+                }
+                <PageBody>
+                    <UserInfoArea>
+                        <UserAvatar source={{uri:userInfo.avatar}} />
+                        <UserInfo>
+                            <UserInfoName> {userInfo.name} </UserInfoName>
+                            <Stars stars={userInfo.stars} showNumber={true} />
+                        </UserInfo>
+                        <UserFavButton>
+                            <FavoriteIcon width="24" height="24" fill="#FF0000"/>
+                        </UserFavButton>
+                    </UserInfoArea>
+
+                    {loading && 
+                        <LoadingIcon size="large" color="#000" />
+                    }
+
+                    <ServiceArea>
+                        <ServicesTitle>Lista de Serviços</ServicesTitle>
+
+                        {userInfo.services.map((item, key)=> (
+                            <ServiceItem>
+                                
+                            </ServiceItem>
+                        ))}
+                    </ServiceArea>
+
+                    <TestimonialArea>
+
+                    </TestimonialArea>
+                </PageBody>
+            </Scroller>
+            <BackButton onPress={handleBackButton}>
+                <BackIcon width="44" height="44" fill="#FFFFFF" />
+            </BackButton>
         </Container>
     );
 }
