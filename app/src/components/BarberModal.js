f4aa7d41064f7c8c9ed905d6126c32425e5b6cce
Jesse <lbarbosa.jessica@gmail.com>
2021-04-03 01:18:52 -0300

    Update BarberModal.js
    finalizando o agendamento

--- app/src/components/BarberModal.js
+++ app/src/components/BarberModal.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components/native';
 import {useNavigation} from '@react-navigation/native';
 
 import ExpandIcon from '../assets/expand.svg';
+import NavPrevIcon from '../assets/nav_prev.svg';
+import NavNextIcon from '../assets/nav_next.svg';
 
 const Modal = styled.Modal``;
 
@@ -67,8 +69,6 @@ const ServicePrice = styled.Text`
     font-weight: bold;
 `;
 
-
-
 const FinishButton = styled.TouchableOpacity`
     background-color: #268596;
     height: 60px;
@@ -83,10 +83,179 @@ const FinishButtonText = styled.Text`
     color: #fff;
 `;
 
+const DateInfo = styled.View`
+    flex-direction: row;
+`;
+
+const DatePrevArea = styled.TouchableOpacity`
+    flex: 1;
+    justify-content: flex-end;
+    align-items: flex-end;
+`;
+
+const DateTitleArea = styled.View`
+    width: 140px;
+    justify-content: center;
+    align-items: center;
+`;
+
+const DateTitle = styled.Text`
+    font-size: 17px;
+    font-weight: bold;
+    color: #000;
+`;
+
+const DateNextArea = styled.TouchableOpacity`
+    flex: 1;
+    justify-content: flex-start;
+    align-items: flex-start;
+`;
+
+const DateList = styled.ScrollView``;
+
+const DateItem = styled.TouchableOpacity`
+    width: 45px;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10px;
+    padding: 5px 0;
+`;
+
+const DateItemWeekDay = styled.Text`
+    font-size: 16px;
+    font-weight: bold;
+`;
+
+const DateItemNumber = styled.Text`
+    font-size: 16px;
+    font-weight: bold;
+`;
+
+const TimeList = styled.ScrollView``;
+
+const TimeItem = styled.TouchableOpacity`
+    width: 75px;
+    height: 40px;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10px;
+`;
+
+const TimeItemText = styled.Text`
+    font-size: 16px;
+    font-weight: bold;
+`; 
+
+const months = [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro'
+];
+
+const days = [
+    'Dom',
+    'Seg',
+    'Ter',
+    'Qua',
+    'Qui',
+    'Sex',
+    'Sab'
+];
+
 
 export default ({show, setShow, user, service}) => {
     
     const navigation = useNavigation();
+
+    const [selectedYear, setSelectedYear] = useState(0);
+    const [selectedMonth, setSelectedMonth] = useState(0);
+    const [selectedDay, setSelectedDay] = useState(0);
+    const [selectedHour, setSelectedHour] = useState(null);
+    const [listDays, setListDays] = useState([]);
+    const [listHours, setListHours] = useState([]);
+
+    useEffect(() => {
+        if (user.available) {
+
+            let daysInMonth = new Date(selectedYear, selectedMonth+1, 0).getDate();
+            let newListDays = [];
+
+            for(let i=1; i<=daysInMonth; i++) {
+                let d = new Date(selectedYear, selectedMonth, i);
+                let year = d.getFullYear();
+                let month = d.getMonth() + 1;
+                let day = d.getDate();
+                month = month < 10 ? '0'+month : month;
+                day = day < 10 ? '0'+day : day;
+                let selDate = `${year}-${month}-${day}`;
+                //let selDate = year+'-'+month+'-'+day;
+
+                let availability = user.available.filter(e=>e.date === selDate);
+
+                newListDays.push({
+                    status: availability.length > 0 ? true : false,
+                    weekday: days[ d.getDay() ],
+                    number: i,
+                });
+            }
+
+            setListDays(newListDays);
+            setSelectedDay(0);
+            setListHours([]);
+            setSelectedHour(0);
+        }
+    }, [user, selectedMonth, selectedYear]);
+
+    useEffect(() => {
+        if(user.available && selectedDay > 0) {
+            let d = new Date(selectedYear, selectedMonth, selectedDay);
+            let year = d.getFullYear();
+            let month = d.getMonth() + 1;
+            let day = d.getDate();
+            month = month < 10 ? '0'+month : month;
+            day = day < 10 ? '0'+day : day;
+            let selDate = `${year}-${month}-${day}`;
+
+            let availability = user.available.filter(e=>e.date === selDate);
+
+            if(availability.length > 0) {
+                setListHours(availability[0].hours);
+            }
+        }
+    }, [user, selectedDay])
+
+    useEffect(() => {
+        let today = new Date();
+        setSelectedYear(today.getFullYear());
+        setSelectedMonth(today.getMonth());
+        setSelectedDay(today.getDate());
+
+    }, []);
+
+    const handleLeftDateClick = () => {
+        let mountDate = new Date(selectedYear, selectedMonth, 1);
+        mountDate.setMonth(mountDate.getMonth() - 1);
+        setSelectedYear(mountDate.getFullYear());
+        setSelectedMonth(mountDate.getMonth());
+        setSelectedDay(0);
+    };
+
+    const handleRightDateClick = () => {
+        let mountDate = new Date(selectedYear, selectedMonth, 1);
+        mountDate.setMonth(mountDate.getMonth() + 1);
+        setSelectedYear(mountDate.getFullYear());
+        setSelectedMonth(mountDate.getMonth());
+        setSelectedDay(0);
+    };
 
     const handleCloseButton = () => {
         setShow(false);
@@ -124,12 +293,71 @@ export default ({show, setShow, user, service}) => {
                         </ModalItem>
                     }
 
+                    <ModalItem>
+                        <DateInfo>
+                            <DatePrevArea onPress={handleLeftDateClick}>
+                                <NavPrevIcon width="35" height="35" fill="#000" />
+                            </DatePrevArea>
 
+                            <DateTitleArea>
+                                <DateTitle>{months[selectedMonth]} {selectedYear}</DateTitle>
+                            </DateTitleArea>
+
+                            <DateNextArea onPress={handleRightDateClick}>
+                                <NavNextIcon width="35" height="35" fill="#000" />
+                            </DateNextArea>
+                        </DateInfo>
+
+                        <DateList horizontal={true} showHorizontalScrollIndicator={false}>
+                            {listDays.map((item, key) => (
+                                <DateItem
+                                    key={key}
+                                    onPress={()=>item.status ? setSelectedDay(item.number) : null}
+                                    style={{
+                                        opacity: item.status ? 1 : 0.5,
+                                        backgroundColor: item.number === selectedDay ? '#4EADBE' : '#fff'
+                                    }}
+                                >
+                                    <DateItemWeekDay
+                                        style={{
+                                            color: item.number === selectedDay ? '#fff' : '#000'
+                                        }}
+                                    >{item.weekday}</DateItemWeekDay>
+                                    <DateItemNumber
+                                        style={{
+                                            color: item.number === selectedDay ? '#fff' : '#000'
+                                        }}
+                                    >{item.number}</DateItemNumber>
+                                </DateItem>
+                            ))}
+                        </DateList>
+                    </ModalItem>
+
+                    {selectedDay > 0 && listHours.length > 0 &&
+                        <ModalItem>
+                            <TimeList horizontal={true} showHorizontalScrollIndicator={false}>
+                                {listHours.map((item, key)=>(
+                                    <TimeItem 
+                                        key={key}
+                                        onPress = {()=>setSelectedHour(item)}
+                                        style={{
+                                            backgroundColor: item === selectedHour ? '#4EADBE' : '#fff'
+                                        }}
+                                    >
+                                        <TimeItemText
+                                            style={{
+                                                color: item === selectedHour ? '#fff' : '#000'
+                                            }}
+                                        >{item}</TimeItemText>
+                                    </TimeItem>
+                                ))}
+                            </TimeList>
+                        </ModalItem>
+                    }
 
                     <FinishButton onPress={handleFinishClick}>
                         <FinishButtonText>Finalizar Agendamento</FinishButtonText>
                     </FinishButton>
-
                 </ModalBody>
             </ModalArea>
         </Modal>
